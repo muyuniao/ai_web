df62932fdffd83210fdd97e4798b4fd38af63dcc
muyuniao <166824356+muyuniao@users.noreply.github.com>
2025-02-13 14:18:07 +0800

    Update MyAiUtils.java

--- src/main/java/com/example/Airtificial/_Intellegence/utill/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package com.example.Airtificial._Intellegence.utill;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
